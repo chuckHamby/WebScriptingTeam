b53df48120e2079720a67387333bb0ac15307339
mjudd-Herz <35943297+mjudd-Herz@users.noreply.github.com>
2018-03-31 10:44:55 -0700

    index
    added a comment on line 2 in index.php

--- sample-ws-master/index.php
+++ sample-ws-master/index.php
@@ -1,5 +1,5 @@
 <?php
-
+//change
 if ($_SERVER["REQUEST_METHOD"] == 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
